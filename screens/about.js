ac7a2debe8d30b9e985e5a747c8049748dd52387
taylorallen <twhitallen07@gmail.com>
2024-09-03 00:02:00 -0500

    test

--- screens/about.js
+++ screens/about.js
@@ -6,6 +6,7 @@ export default function About({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>About Screen</Text>
+            <Text>This is an app that I'm working on to build my React Native skills.</Text>
             <Button
         onPress={() => navigation.navigate('Home')}
         title="Return to Home Screen"
